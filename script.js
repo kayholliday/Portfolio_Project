d7a4bd8a683dc90b82efcde8714481862a3a488f
Kay_H <kayholliday@ymail.com>
2024-04-11 14:44:02 +0100

    Styling first draft

---
+++ script.js
@@ -0,0 +1,6 @@
+let projectColourChange = document.getElementsByClassName('project');
+let mouseOverFunction = () => {
+    projectColourChange.style.backgroundColor = '#a4b8ac';
+};
+
+projectColourChange.onmouseover = mouseOverFunction;
